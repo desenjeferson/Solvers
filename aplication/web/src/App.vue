09c2dd2c3b4bdd2d90a19ae0223177207af6d808
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-07-29 19:02:23 -0300

    Initial commit

--- aplication/web/src/App.vue
+++ aplication/web/src/App.vue
@@ -1,62 +1,41 @@
 <template>
   <div id="app">
     <div v-if="!isSignIn">
-      <input v-model="login" @click="singIn(login)"/>
+      <input v-model="login" @click="singIn(login)" />
       <button @click="singIn(login)">Sing in</button>
     </div>
     <div v-if="isSignIn">
-      <div id="nav">
-        <router-link to="/">Home</router-link>|
-        <router-link to="/about">About</router-link>
-      </div>
-      {{user}}
-      <router-view />
+      <Home :user="user" />
     </div>
   </div>
 </template>
 
 <script>
 import User from "./services/user";
+import Home from "./components/Home.vue";
+
 export default {
+  name: "App",
+  components: {
+    Home
+  },
   data: () => ({
     isSignIn: false,
     login: "",
-    user:[]
+    user: []
   }),
   mounted() {
+    this.singIn(1);
   },
   methods: {
     singIn(userID) {
       User.listar(userID).then(resposta => {
-        if (resposta.status===200){
+        if (resposta.status === 200) {
           this.isSignIn = true;
-          this.user=resposta.data;
+          this.user = resposta.data;
         }
       });
     }
   }
 };
-</script>>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ aplication/web/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="user">
+    <img :src="user.avatar">
+    <h1>{{ user.name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+    user: []
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user{
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
