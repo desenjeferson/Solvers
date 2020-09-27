4d0c856aee8035b8e6a2f7ab1d93663e86bc583c
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-27 17:57:00 -0300

    Init Project

--- aplication/web/src/services/config.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios'
-
-export const http = axios.create({
-    baseURL: 'https://5f1b072e610bde0016fd34be.mockapi.io/'
-})
--- aplication/web/src/services/user.js
+++
@@ -1,9 +0,0 @@
-import { http } from './config'
-
-export default {
-    listar: (userID) => {
-        if (!userID) return false;
-        const user = 'users/' + userID
-        return http.get(user)
-    }
-}
