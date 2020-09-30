2d94bbd88a169b0561064cd104a7540f941db901
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-29 22:27:48 -0300

    Feat: Create cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,63 @@
 <template>
   <div>
-    <v-row>
-      <v-col v-for="(project, indice) in projects" :key="indice" cols="4">
-        <v-card height="200">
-          {{project.name}}
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-container fluid>
+      <Waterfall :options="null">
+        <WaterfallItem v-for="(project, indice) in projects" :key="indice">
+          <CardProject :project="project" />
+        </WaterfallItem>
+      </Waterfall>
+    </v-container>
   </div>
 </template>
 
 <script>
+import CardProject from "../components/Card-Project.vue";
+import { Waterfall, WaterfallItem } from "vue2-waterfall";
+
 export default {
+  components: {
+    CardProject,
+    Waterfall,
+    WaterfallItem,
+  },
   data() {
     return {
-      projects: [{name: 'War Base'}]
+      projects: [
+        {
+          name: "War Base",
+          status: "In progress",
+          descripition: "Uno joguinho",
+          image:
+            "https://i.pinimg.com/736x/af/96/76/af96763ed2bfeee4141a8515cc39e6a7.jpg",
+
+          timeLine: {
+            title: "What is this",
+            times: [
+              {
+                message: "Um jogo de guerra",
+                color: "green lighten-1",
+              },
+              {
+                message: "Bases",
+                color: "lime lighten-1",
+              },
+            ],
+          },
+          bunttons: [
+            {
+              name: "GitHub",
+              link: "https://github.com/desenjeferson/war-base",
+              icon: "mdi-github",
+            },
+            {
+              name: "More",
+              link: "https://github.com/desenjeferson/war-base",
+              icon: "mdi-plus",
+            },
+          ],
+        },
+      ],
     };
-  }
-}
+  },
+};
 </script>
